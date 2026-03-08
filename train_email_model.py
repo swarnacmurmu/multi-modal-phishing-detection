1a2b6ba040c3bcc22d95879996604931f4a48de7
codenot_04 <swarna.c.murmu04@gmail.com>
2026-03-09 01:51:50 +0530

    Add files via upload

---
+++ train_email_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("data/processed/email_clean.csv")
+
+X = data["text_combined"]
+y = data["label"]
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, pred))
+joblib.dump(model, "models/email_model.pkl")
